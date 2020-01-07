30c2813dab44ccf067504382ec2f18c482fbaafa
Jorge Octavio <jorgeo80@gmail.com>
2020-01-07 15:47:59 -0600

    :panda_face: Repaso-JS commit 1.3

--- Repaso-JS/js/main.js
+++ Repaso-JS/js/main.js
@@ -1,14 +1,40 @@
 var arrayPost = []
 
+const htmlCard = (postTitle, postDate, postArt, postName, n = 1) => {
+    return (
+      `
+      <div class="card col-md-4 col-12" style="width: 18rem;">
+        <img class="card-img-top" src="https://picsum.photos/1440/600?random=${n}" alt="Card image cap">  
+        <div class="card-body">
+            <h5 class="card-title">${postTitle}</h5>
+            <p class="card-text">${postDate}</p>
+            <p class="card-text">${postArt}</p>
+            <p class="card-text">${postName}</p>
+        </div>
+      </div>
+      `
+    )
+  }
+
+  const  buildCard= (cards, parent) => {
+    var accCards = cards.reduce((acc, {Title, scrImg, Name, Post, pDate}, cIx)=> {
+      console.log(acc)
+      return acc + htmlCard(Title, Name, Post, pDate, cIx) 
+    }, '')
+    document.getElementById(parent).innerHTML = accCards
+  }
+
+
 const submit = document.getElementById("Submit")
 const addPost = () => {
-    return arrayPost.push({
+    arrayPost.push({
         Title: document.getElementById("Title").value,
-        Img: document.getElementById("Image").value,
+        scrImg: document.getElementById("Image").value,
         Name: document.getElementById("Name").value,
         Post: document.getElementById("Post").value,
-        Date: document.getElementById("Date").value
-})
+        pDate: document.getElementById("Date").value
+    })
+    buildCard(arrayPost,"postBuilder")
 }
 submit.addEventListener("click",addPost)
 
