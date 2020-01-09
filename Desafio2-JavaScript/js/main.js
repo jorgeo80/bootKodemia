1554831a660dd44ffe31c18a877c36e9721d73e0
Jorge Octavio <jorgeo80@gmail.com>
2020-01-08 19:20:57 -0600

    :v: commit 1.7

--- Desafio2-JavaScript/js/main.js
+++ Desafio2-JavaScript/js/main.js
@@ -1,46 +1,52 @@
+var nPlay = 4
+var randomArray = []
+var answerArray = []
+var clickString = ""
 
-// let randomArray = []
-// let answerArray = []
-
-// for (let i = 1; i <= 3; i++){
-//     setTimeout(() => {
-//         randomArray.push(Math.trunc(Math.random()*12+1))
-//         console.log(randomArray)
-//     },3000)
-// }
-// console.log(randomArray)
-
-// document.addEventListener("click", 
-// (buttonValue) => {
-//     answerArray.push(parseInt(buttonValue.toElement.value))
-//     console.log(buttonValue.toElement.value, answerArray)
-// })
-
-// console.log(randomArray == answerArray)
-
-
-var numMax = 12
-var setTimmer = 1000
-var n = 12
-const start = (n = 1, randomArray = []) => {
+const start = (n, arr = [], setTimmer = 1000, numMax = 12) => {
     if(n > 0){
         n--
         setTimeout(function(){
             let randomNum = Math.trunc(Math.random() * numMax + 1)
             let onButton = document.querySelector(`button[value="${randomNum}"]`)
             setTimeout(() => {
-                if (randomArray.length > 0){
+                if (arr.length > 0){
                     let offButton = document.querySelector(`button[value="${
-                        randomArray[randomArray.length-1]}"]`)
+                        arr[arr.length-1]}"]`)
                     offButton.textContent = ""        
                 }
             }, (setTimmer * 0.75))
             onButton.textContent = randomNum
-            randomArray.push(randomNum)            
-            start(n, randomArray)
-            console.log(randomArray, n)            
+            arr.push(randomNum)            
+            start(n, arr)
+            console.log(arr, n)
+            return arr
         }, (setTimmer * 1.25))
-    }
+    }    
 }
-start(n)
+
+
+const answers = (buttonValue) => {
+    if(parseInt(buttonValue.toElement.value) == buttonValue.toElement.value){
+        answerArray.push(parseInt(buttonValue.toElement.value))
+    }
+    console.log(buttonValue.toElement.value, answerArray)
+    return answerArray
+}    
+
+const valueClick = buttonValue => clickString = buttonValue.toElement.value    
+
+document.addEventListener("click", answers)
+document.addEventListener("click", valueClick)
+document.addEventListener("click", () =>{
+    if(clickString === "start"){
+        randomArray = []
+        answerArray = []
+        start(nPlay, randomArray)
+    }    
+})
+
+
+//console.log(randomArray == answerArray)
+
 
