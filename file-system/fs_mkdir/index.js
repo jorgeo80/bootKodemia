f92d57b511816f2e0553f4d7a434ad8c67bfb80f
Jorge Octavio <jorgeo80@gmail.com>
2020-01-29 09:33:28 -0600

    :monkey: mkdir js

--- file-system/fs_mkdir/index.js
+++ file-system/fs_mkdir/index.js
@@ -1,6 +1,12 @@
 const fs = require('fs')
 
-fs.mkdir('test1', { recursive: true }, err => {
-    if(err) throw err 
-    console.log('Carpeta Creada')
-})
+function makeDirFile(pathDir,fileName){
+    fs.mkdir(pathDir, { recursive: true }, err => {
+        if(err) throw err 
+        console.log('Carpeta Creada')
+    })
+    fs.writeFile(pathDir + '/' + fileName,'Escritura de Prueba',
+        err => err || console.log(`Salida`))
+}
+
+makeDirFile('test2','writeTest2.txt')
