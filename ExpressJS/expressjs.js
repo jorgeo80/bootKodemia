db152e852f68951def71df04ff9f888a74c96c52
Jorge Octavio <jorgeo80@gmail.com>
2020-02-04 21:31:36 -0600

    :honeybee: ExpressJS

--- ExpressJS/expressjs.js
+++ ExpressJS/expressjs.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const server = express()
 
-server.get('/', (req, res) => {
-    res.send('No eres Jorge')    
-})
-
 server.get('/jorge', (req, res) => {
     res.send('Toma un Jorge')    
 })
 
 server.post('/jorge', (req, res) => {
     res.send('Crea un Jorge')    
+})
+
+server.all('*', (req, res) => {
+    res.send('No eres Jorge')    
 })
 
 server.listen(8080, () => {
