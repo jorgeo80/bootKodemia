ed219a67b93610de1488ef29e33bae0437f086bf
Jorge Octavio <jorgeo80@gmail.com>
2020-02-04 21:50:26 -0600

    :honeybee: ExpressJS

--- ExpressJS/expressjs.js
+++ ExpressJS/expressjs.js
@@ -9,6 +9,34 @@ server.post('/jorge', (req, res) => {
     res.send('Crea un Jorge')    
 })
 
+server.get('/middelwares', (req, res, next) => {
+    console.log('Middleware 1')
+    next()
+}, (req, res, next) => {
+    console.log('Middleware 2')
+    next()
+}, (req, res, next) => {
+    console.log('Middleware 3')
+    next()
+}, (req, res) => {
+    console.log('fin')
+    res.send('fin')
+})
+
+server.use((req, res, next) => {
+    console.log('Middleware NAME')
+    next()
+})
+
+server.get('/koders/:id', (req, res) => {
+    resp.send(`Koder: ${req.params.id}`)
+})
+
+server.get('/jorge/name', (req, res) => {
+    res.send('Jorge')    
+})
+
+
 server.all('*', (req, res) => {
     res.send('No eres Jorge')    
 })
