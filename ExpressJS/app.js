43f86d7efe1904112982fe1232120fb65d39efe9
Jorge Octavio <jorgeo80@gmail.com>
2020-02-04 18:19:10 -0600

    :honeybee: ExpressJS

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -1,12 +1,12 @@
 
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.get('/', (request, response) => {
+  response.send('Hello World!')
+})
 
-app.listen(8080, function () {
+app.listen(8080, () => {
   console.log('Example app listening on port 8080!');
-});
+})
 
