8e50bf86d1c8917383a78c00daa8ec5316464bd1
Jorge Octavio <jorgeo80@gmail.com>
2020-02-06 16:31:36 -0600

    :monkey: Express Koders-API

--- koders-api/index.js
+++ koders-api/index.js
@@ -2,24 +2,60 @@ const express = require('express')
 const app = express()
 const koderslist = require('./kodersList.js') // TODO: create a file
 
+app.use(express.json()) // esta funcion define el body
+
+// app.post('/', (request, response) => {
+//   console.log('body: ', request.body)
+//   response.json(request.body)
+// })
+
+app.post('/koders', (request, response) => {
+  const koder = request.body
+  koderslist.push(koder)  
+  response.json({
+    success: true,
+    message: 'Koder created',
+    data: {
+      koder
+    }
+  })
+})
+
 app.get('/koders', (request, response) => {
+  const { name } = request.query
+  console.log('name :',name )
+  let koders = null
+  if (name) {
+    koders = koderslist.filter(koder => koder.name.includes(name))
+  }
   response.json({
     success: true,
     data: {
-      koders: koderslist
+      koders: koders || koderslist
     }
   })
 })
 
 app.get('/koders/:id', (request, response) => {
+  try {
   const { id: koderId } = request.params
   const koder = koderslist.find((koder => koder.id === parseInt(koderId)))
+  
+  if (!koder) throw new Error(`Can't find koder with id ${koderId}`)
   response.json({
     success: true,
     data: {
       koder
     }
   })
+} catch(error) {
+  response.status(404)
+  response.json({
+    success: true,
+    error: error.message
+  })
+
+}
 })
 
 app.get('/koders/name/:name', (request, response) => {
