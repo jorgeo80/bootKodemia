3e7dce122866a5f4f22328ce424958118fb043aa
Jorge Octavio <jorgeo80@gmail.com>
2020-02-20 19:23:15 -0600

    :racehorse: React Clase 1

---
+++ React-ClaseI/ClassExample.js
@@ -0,0 +1,15 @@
+class Tirangle {
+  constructor(side1, side2, side3) {
+    this.side1 = side1
+    this.side2 = side2
+    this.side3 = side3
+  }
+}
+
+const tirangle1 = new Tirangle(10, 10, 10)
+const tirangle2 = new Tirangle(11, 11, 11)
+const tirangle3 = new Tirangle(12, 12, 12)
+
+console.log('Triangle 1: ', tirangle1)
+console.log('Triangle 2: ', tirangle2)
+console.log('Triangle 3: ', tirangle3)
---
+++ React-ClaseI/User.js
@@ -0,0 +1,9 @@
+class User {
+  constructor(Name, Age, Email){
+    this.Name = Name
+    this.Age = Age
+    this.Email = Email
+
+  }
+
+}
