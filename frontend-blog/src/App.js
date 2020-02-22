b71cf4ef34477439ea373b43b2140e9667e64f55
Jorge Octavio <jorgeo80@gmail.com>
2020-02-21 20:59:45 -0600

    :wolf: React Frontend Blog

--- frontend-blog/src/App.js
+++ frontend-blog/src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import './assets/styles/App.css';
+import React from 'react'
+import Navbar from './components/Navbar'
+import Main from './components/Main'
+import Footer from './components/Footer'
+//import './assets/styles/App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Main />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
