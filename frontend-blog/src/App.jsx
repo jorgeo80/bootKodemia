43ec8e31eb246982493469c65791756712020829
Jorge Octavio <jorgeo80@gmail.com>
2020-02-25 09:49:01 -0600

    :wolf: React Frontend Blog

--- frontend-blog/src/App.jsx
+++ frontend-blog/src/App.jsx
@@ -1,11 +1,87 @@
 import React from 'react'
+
 import Navbar from './components/Navbar'
-// import './assets/styles/App.css'
+
+import PostCard from './components/PostCard'
+import AppButton from './components/AppButton'
 
 function App () {
+  const numPosts = [1, 2, 3];
+  const imgArray = numPosts.map(val => {
+    return `https://picsum.photos/id/${Math.trunc(Math.random() * val * 10)}/200/300`
+  })
+  const readTimeArray = numPosts.map(val => `${Math.trunc(Math.random() * val * 10)}m`)
+
   return (
-    <div className='App'>
+    <div className='app'>
       <Navbar />
+
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-4'>
+            <PostCard
+              img={imgArray[0]}
+              text={`Prueba Texto ${Math.trunc(Math.random() * 10)}`}
+              readTime={readTimeArray[0]}
+            >
+              <div className='btn-group'>
+                <AppButton
+                  text='Edit'
+                  size='sm'
+                  className='btn-outline-secondary'
+                />
+                <AppButton
+                  text='View'
+                  size='sm'
+                  className='btn-outline-secondary'
+                />
+              </div>
+            </PostCard>
+          </div>
+
+          <div className='col-md-4'>
+            <PostCard
+              img={imgArray[1]}
+              text={`Prueba Texto ${Math.trunc(Math.random() * 10)}`}
+              readTime={readTimeArray[1]}
+            >
+              <div className='btn-group'>
+                <AppButton
+                  text='Edit'
+                  size='sm'
+                  className='btn-outline-secondary'
+                />
+                <AppButton
+                  text='View'
+                  size='sm'
+                  className='btn-outline-secondary'
+                />
+              </div>
+            </PostCard>
+          </div>
+
+          <div className='col-md-4'>
+            <PostCard
+              img={imgArray[2]}
+              text={`Prueba Texto ${Math.trunc(Math.random() * 10)}`}
+              readTime={readTimeArray[2]}
+            >
+              <div className='btn-group'>
+                <AppButton
+                  text='Edit'
+                  size='sm'
+                  className='btn-outline-secondary'
+                />
+                <AppButton
+                  text='View'
+                  size='sm'
+                  className='btn-outline-secondary'
+                />
+              </div>
+            </PostCard>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- frontend-blog/src/components/Navbar/NavbarToggler.jsx
+++ frontend-blog/src/components/Navbar/NavbarToggler.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 function NavbarToggler () {
   return (
-    <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarsExampleDefault' aria-controls='navbarsExampleDefault' aria-expanded='false' aria-label='Toggle navigation'>
-      <span className='navbar-toggler-icon'/>
+    <button
+      className='navbar-toggler'
+      type='button'
+      data-toggle='collapse'
+      data-target='#navbarsExampleDefault'
+      aria-controls='navbarsExampleDefault'
+      aria-expanded='false'
+      aria-label='Toggle navigation'
+    >
+      <span
+        className='navbar-toggler-icon'
+      />
     </button>
   )
 }
